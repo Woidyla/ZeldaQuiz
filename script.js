f85b3c24cf1c421ae95a56d140b6b2231291cb66
Woidyla <142675593+Woidyla@users.noreply.github.com>
2023-10-21 04:08:18 +0000

    files

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
+$("#quiz1").dlxQuiz({
+    quizData: {
+        questions: [
+            {
+                q:
+                    'In which year did the first game release?',
+                a: "1986",
+                options: [
+                    "1988",
+                    "1990",
+                    "1986",
+                    '1991'
+                ]
+            },
+            {
+                q: "What was the title of the 5th game in the series?",
+                a: "Ocarina of Time",
+                options: [
+                    "Majora's Mask",
+                    "Ocarina of Time",
+                    "Twilight Princess",
+                "A Link to the Past"    
+                ]
+            },
+            {
+                q: "How many masks are there in Majora's Mask?",
+                a: "24",
+                options: [
+                    "21", 
+                    "26",
+                    "29",
+                    "24"
+                ]
+            },
+            {
+                q: "In the Wind Waker, where was Farore's Pearl found?",
+                a: "Forest Haven",
+                options: [
+                    "Dragon Roost Island",
+                    "Forest Haven",
+                    'Outset Island'
+                ]
+            },
+            {
+                q:
+                    "What is the heros name in the series?",
+                a: "Link",
+                options: [
+                    "Zelda",
+                    "Link",
+                    "Ganon",
+                    "Impa"
+                ]
+            }
+        ]
+    }
+});
 
